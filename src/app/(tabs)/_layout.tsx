ff094bfcf6466009508e72b9b02f7c22f0c3d65a
Vitor Cyriaco <vitorcyriaco@outlook.com>
2025-10-28 19:22:29 -0300

    ajuste das rotas

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,9 +1,68 @@
-import { Stack } from "expo-router";
+import { Feather } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import { View } from "react-native";
 
 export default function TabLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-    
-    </Stack>
+    <Tabs screenOptions={() => ({
+      tabBarShowLabel: false,
+      tabBarStyle: {
+        height: 63,
+        borderTopWidth: 0,
+      },
+      tabBarHideOnKeyboard: true,
+      headerShown: false
+    })}>
+      <Tabs.Screen name="index"
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <View style={{ alignItems: "center" }}>
+              <View style={iconContainerStyle}>
+                <Feather name="users" size={30} color={color} />
+              </View>
+              {focused && (
+                <View style={{ width: 20, height: 4, borderRadius: 2, marginBottom: 3, }} />
+              )}
+            </View>
+          ),
+        }}
+      />
+      <Tabs.Screen name="liked"
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <View style={{ alignItems: "center" }}>
+              <View style={iconContainerStyle}>
+                <Feather name="users" size={30} color={color} />
+              </View>
+              {focused && (
+                <View style={{ width: 20, height: 4, borderRadius: 2, marginBottom: 3, }} />
+              )}
+            </View>
+          ),
+        }}
+      />
+      <Tabs.Screen name="match"
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <View style={{ alignItems: "center" }}>
+              <View style={iconContainerStyle}>
+                <Feather name="users" size={30} color={color} />
+              </View>
+              {focused && (
+                <View style={{ width: 20, height: 4, borderRadius: 2, marginBottom: 3, }} />
+              )}
+            </View>
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
+
+const iconContainerStyle = {
+  width: 50,
+  height: 50,
+  justifyContent: "center" as const,
+  alignItems: "center" as const,
+  marginTop: 20,
+};
---
+++ src/app/(tabs)/match.tsx
@@ -0,0 +1,11 @@
+import { Match } from "@/components/match";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function MatchTab() {
+    return (
+        <SafeAreaView>
+            <Match />
+        </SafeAreaView>
+
+    )
+}
